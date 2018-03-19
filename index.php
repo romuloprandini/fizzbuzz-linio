991a9746c716ed980c71673fdf6c5d17177bb15b
Rômulo Prandini Lima <romuloprandini@gmail.com>
2018-03-19 17:41:25 -0400

    Oops fix size

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 function fizzbuzz($size) { 
-    foreach (range(1, 100) as $number) { 
+    foreach (range(1, $size) as $number) { 
         echo((array(0 => 'Linianos', 1=> 'IT', 2 => 'linio')[ceil(($number % 3) / 10) + 2 * ceil(($number % 5) / 10)] ?? $number).' '); 
     }
 } 
