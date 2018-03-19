a68fd05b81e872c4aa94adb5d83a1ffa2dbd7d9f
Rômulo Prandini Lima <romuloprandini@gmail.com>
2018-03-19 17:17:02 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
 <?php 
-$words = [0 => 'Linianos', 1=> 'IT', 2 => 'linio'];
 
 function mod($number, $divider) {
     return ceil(($number % $divider) / 10);
 }
 
-for ($number = 1; $number <= 100; $number++) {
-    
-    $multiple = mod($number,3) + 2 * mod($number,5);
-    
-    if($multiple <= 2) {
-        echo($words[$multiple].' ');
-        continue;
-    }
+function fizzbuzz($size) {
+    $words = [0 => 'Linianos', 1=> 'IT', 2 => 'linio'];
+    for ($number = 1; $number <= 100; $number++) {
 
-    echo($number.' ');
+        $multiple = mod($number,3) + 2 * mod($number,5);
+
+        if($multiple <= 2) {
+            echo($words[$multiple].' ');
+            continue;
+        }
+
+        echo($number.' ');
+    }
 }
+
+fizzbuzz(100);
